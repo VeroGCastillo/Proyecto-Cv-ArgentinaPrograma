e161c1486960f495f0dbadb5bfbb6ab8f187cd5c
veronica <veckycastillo@hotmail.com>
2023-01-31 03:58:38 -0300

    first commit

---
+++ cv.js
@@ -0,0 +1,8 @@
+document.getElementById("myBtn").addEventListener("click", function () {
+  alert(
+    "Hola Soy Darlene Ward, soy desarrolladora web. Te invito a conocer mi CV."
+  );
+});
+document.getElementById("myBtn2").addEventListener("click", function () {
+  alert("Te invito a seguirme en mis redes sociales");
+});
